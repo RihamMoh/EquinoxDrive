bd3bc3ed24343c5bcb228e552089d451e2d80784
A M H Riham Mohamed <88776640+RihamMoh@users.noreply.github.com>
2023-06-02 22:28:41 +0530

    Update run.sh
    Signed-off-by: A M H Riham Mohamed <88776640+RihamMoh@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # For developing purpose
 # When developing and not for docker
 
-npm run start --prefix api/javascript/api/ &&
+npm run start --prefix api/javascript/api/ &
 npm run start --prefix app/
